6d99855fcd3c5eda009bfb1092dbf5266d2f62b7
TheAssassin <theassassin@assassinate-you.net>
2018-07-21 15:17:49 +0200

    Add wrapper for Makefile managing a virtualenv in the source dir

---
+++ build.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+CUR_DIR=$(readlink -f $(dirname "$0"))
+
+cd "$CUR_DIR"
+
+VENV="$CUR_DIR"/.venv
+
+if [ ! -d "$VENV" ] && [ ! -f "$VENV"/bin/activate ]; then
+    echo $(tput bold)$(tput setaf 2)"Creating new virtual environment in $VENV"$(tput sgr0)
+    python3 -m venv "$VENV"
+fi
+
+source "$VENV"/bin/activate
+
+# this snippet should allow us to call pip install only if the requirements file has been touched
+if [ ! -f "$VENV"/requirements.txt ] || [ $(sha256sum requirements.txt | cut -d' ' -f1) != $(sha256sum "$VENV"/requirements.txt | cut -d' ' -f1) ]; then
+    echo $(tput bold)$(tput setaf 2)"Requirements updated, reinstalling"$(tput sgr0)
+    cp requirements.txt "$VENV"/requirements.txt
+    pip install -r "$VENV"/requirements.txt
+fi
+
+make "$@"
