0ec3997d63b32f06b563cf90d0d6216808391426
TheAssassin <theassassin@assassinate-you.net>
2018-07-22 20:06:19 +0200

    Fix latex generation

--- conf.py
+++ conf.py
@@ -114,7 +114,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'AppImagedocumentationdoc'
+htmlhelp_basename = 'AppImagedoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -122,15 +122,15 @@ htmlhelp_basename = 'AppImagedocumentationdoc'
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
-    # 'papersize': 'letterpaper',
+    'papersize': 'a4paper',
 
     # The font size ('10pt', '11pt' or '12pt').
     #
-    # 'pointsize': '10pt',
+    'pointsize': '10pt',
 
     # Additional stuff for the LaTeX preamble.
     #
-    # 'preamble': '',
+    #'preamble': r'\usepackage{inputenc}',
 
     # Latex figure (float) alignment
     #
@@ -141,7 +141,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'AppImagedocumentation.tex', 'AppImage documentation Documentation',
+    (master_doc, 'AppImage.tex', 'AppImage Documentation',
      'AppImage project', 'manual'),
 ]
 
@@ -151,7 +151,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'appimagedocumentation', 'AppImage documentation Documentation',
+    (master_doc, 'AppImage', 'AppImage Documentation',
      [author], 1)
 ]
 
@@ -162,8 +162,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'AppImagedocumentation', 'AppImage documentation Documentation',
-     author, 'AppImagedocumentation', 'One line description of project.',
+    (master_doc, 'AppImage', 'AppImage Documentation',
+     author, 'AppImage', 'One line description of project.',
      'Miscellaneous'),
 ]
 
