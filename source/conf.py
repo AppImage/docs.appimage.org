b543ed826dd611923e9d76b7f379d7bdf2722caa
TheAssassin <theassassin@assassinate-you.net>
2018-07-23 20:57:07 +0200

    Add favicon

--- source/conf.py
+++ source/conf.py
@@ -102,6 +102,8 @@ html_context = {
     "github_version": "master/source/"
 }
 
+html_favicon = "_static/favicon.ico"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
