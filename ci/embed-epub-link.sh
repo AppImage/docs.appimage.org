f9c7a6774fbebac74318395f6d1dd15119a18985
TheAssassin <theassassin@assassinate-you.net>
2020-12-07 02:56:02 +0100

    Embed .epub link on front page

---
+++ ci/embed-epub-link.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+set -ex
+
+epub_filename="$1"
+
+if [ -f "$epub_filename" ]; then
+    echo "Usage: bash $0 <epub filename>"
+    exit 2
+fi
+
+# insert download link on frontpage
+# note that we can't use \EOF easily, since we need to embed the filename
+cat >> source/index.rst <<\EOF
+
+
+Download this document
+----------------------
+
+`Download as ePub <download/epub_filename>`_
+EOF
+
+sed -i "s|epub_filename|$epub_filename|" source/index.rst
